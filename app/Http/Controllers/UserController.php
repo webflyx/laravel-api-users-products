68102f4a8b07f65772ca4dd795031c2ad83ab03b
webflyx <zavala180395@gmail.com>
2023-11-25 18:07:51 +0200

    Update user migration. Set api/users

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::with('products')->get();
+        return UserResource::collection($users);
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(User $user)
+    {
+        //
+    }
+
+    public function update(Request $request, User $user)
+    {
+        //
+    }
+
+    public function destroy(User $user)
+    {
+        //
+    }
+}
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->decimal('amount', 16, 2)->default(0);
+            $table->string('avatart')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
